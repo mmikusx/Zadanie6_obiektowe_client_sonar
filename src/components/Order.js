d6ab49e578fa8038e315fbbdfcca9288b4aa2a41
mmikusx <73282940+mmikusx@users.noreply.github.com>
2024-05-08 01:10:56 +0200

    get rid of issues
    - Remove this useless assignment to variable "services".
    - Remove this unused assignment to variable "services".
    - Do not use Array index in keys

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,21 +2,21 @@ import React, { useContext } from 'react';
 import { OrderContext } from '../OrderContext';
 
 function Order() {
-    const [orderItems, , removeFromOrder, services] = useContext(OrderContext);
+    const { orderItems, removeFromOrder, services } = useContext(OrderContext);
     const totalCost = orderItems.reduce((total, item) => total + item.service.price * item.quantity, 0);
 
     return (
         <div>
             <h1>Zamówienie</h1>
             <ul>
-                {orderItems.map((item, index) => (
-                    <li key={index}>
-                        {item.service.name} - ${item.service.price} x {item.quantity} = ${item.service.price * item.quantity}
+                {orderItems.map((item) => (
+                    <li key={item.service.id}>
+                        {item.service.name} - {item.service.price} x {item.quantity} = {item.service.price * item.quantity}
                         <button onClick={() => removeFromOrder(item.service)}>Usuń z zamówienia</button>
                     </li>
                 ))}
             </ul>
-            <h2>Całkowity koszt: ${totalCost}</h2>
+            <h2>Całkowity koszt: {totalCost}</h2>
         </div>
     );
 }
